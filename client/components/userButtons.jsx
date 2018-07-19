159d3704e5c3c80bafae31e4931278174014ed85
ansonao <ansonao10@gmail.com>
2018-07-19 15:36:21 -0700

    Obtain icons from Material Icons, put icons for user buttons.

--- client/components/userButtons.jsx
+++ client/components/userButtons.jsx
@@ -8,11 +8,11 @@ class UserButtons extends React.Component {
 	render() {
 		return (
 			<div className="content-user-buttons">
-				<button className="reviewButton">Write a Review</button>
+				<button className="reviewButton"><i class="material-icons">star</i><span className="buttonText">Write a Review</span></button>
 				<div className="spanDiv">
-					<button className="buttonSpans">Add Photo</button>
-					<button  className="buttonSpans">Share</button>
-					<button  className="buttonSpans">Save</button>
+					<button className="buttonSpans"><i class="material-icons">photo_camera</i><span className="buttonText">Add Photo</span></button>
+					<button className="buttonSpans"><i class="material-icons">share</i><span className="buttonText">Share</span></button>
+					<button className="buttonSpans"><i class="material-icons">save</i><span className="buttonText">Save</span></button>
 				</div>
 			</div>
 		)
