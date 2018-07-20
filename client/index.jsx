0b71107689ee84bd0c105de39edc65196904bc4a
ansonao <ansonao10@gmail.com>
2018-07-20 15:53:54 -0700

    Change index.jsx to suit the style of eslint

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,8 @@
 /* eslint-env browser */
+/* eslint no-else-return: "error" */
 import React from 'react';
 import ReactDom from 'react-dom';
+import $ from 'jquery';
 import MapView from './components/mapView';
 import MapInfo from './components/mapInfo';
 import RestInfo from './components/restInfo';
@@ -10,31 +12,72 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hello: 'hello',
+      name: 'in-n-out',
+      address: '',
+      phone: '',
+      website: ''
     };
   }
 
+  getInfo() {
+    const { name } = this.state;
+    $.ajax({
+      url: `http://localhost:3001/biz/${name}/info`,
+      method: 'GET',
+      dataType: 'json',
+    }).then((response) => {
+      console.log(response);
+      const latlng = {
+        lat: response.lat,
+        lng: response.lng,
+      };
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ location: latlng }, (results, status) => {
+        if (status === 'OK') {
+          console.log(results[0].formatted_address);
+          // this.setState({
+          //   address: results[0].formatted_address,
+          //   phone: response.phone,
+          //   website: response.website
+          // })
+          this.setState({
+            address: results[0].formatted_address,
+            phone: response.phone,
+            website: response.website
+          })
+        } else {
+          console.log('not ok');
+        }
+      });
+      // console.log(geocoder);
+    }, (err) => {
+      console.log(err);
+      return err;
+    });
+  }
+
   render() {
-    const { hello } = this.state;
+    const {address, phone, website} = this.state;
     return (
       <div className="top-container">
         <div className="top-content-container">
           <div className="content-header-container">
-            <p>
-              {hello}
-            </p>
             <RestInfo />
             <UserButtons />
           </div>
           <div className="content-body-container">
             <div className="content-map-info">
               <MapView />
-              <MapInfo />
+              <MapInfo address={address} phone={phone} website={website}/>
             </div>
           </div>
         </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.getInfo();
   }
 }
 
