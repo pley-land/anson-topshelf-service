1f3043bc9887943e457e4b6499cd2235220fe45b
ansonao <ansonao10@gmail.com>
2018-07-30 20:32:55 -0700

    Trial No.10

--- client/__tests__/indexTest.jsx
+++ client/__tests__/indexTest.jsx
@@ -3,6 +3,8 @@ import renderer from 'react-test-renderer';
 import React from 'react';
 import App from '../components/renderApp';
 
+console.log(BASH_ENV);
+
 describe('test App', () => {
   it('should render App properly', () => {
     const output = renderer.create(<App />).toJSON();
